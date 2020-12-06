af2122952dc46d149b262e90844590039774893a
Sailesh Pant <saileshpant12@gmail.com>
2020-12-06 09:50:45 +0545

    email and adminlte integration

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('test-email', function () {
+    $name = 'Sailesh Pant Changed';
+    return new App\Mail\MarkdownEmail($name);
+//   \Illuminate\Support\Facades\Mail::to('saileshpant12@gmail.com')->send(new \App\Mail\MarkdownEmail($name));
+});
+
+Route::get('dashboard', function () {
+   return view('dashboard');
+});
+
+Route::get('create-blog', function () {
+   return view('create-blog');
+});
 
 Auth::routes();
 
